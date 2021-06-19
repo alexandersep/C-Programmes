53e52aa5ba7f3cd0fbd0202e9d0763d6701a6ecd
alexandersep <sepelena@tcd.ie>
2021-06-19 21:19:00 +0100

    quicksorth-comments

--- quicksort/quicksort.h
+++ quicksort/quicksort.h
@@ -4,10 +4,13 @@
 /* takes pointer/array and lo and hi index of portion of the array */ 
 void quicksort(int *pArray, int lo, int hi); 
 
+/* used in quick sort to seperate elements into groups */
 int partition(int *pArray, int lo, int hi);
 
+/* used in quick sort to swap two elements in the array */
 void swap(int *pArray, int aIndex, int bIndex);
 
+/* print values in the array */
 void printArray(signed int * pArray, int numOfElements);
 
 #endif
